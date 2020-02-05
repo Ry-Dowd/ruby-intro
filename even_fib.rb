e8f8c9922deba6795895214f70cc7c499a5fa547
Ryan <rydowd@gmail.com>
2020-02-05 16:21:55 -0500

    Program now outputs all numbers in the fibonacci sequence less than 4 million and adds the even ones

--- even_fib.rb
+++ even_fib.rb
@@ -1,10 +1,10 @@
-fibonacci_hash = { 0 => 0, 1 => 1 }
+$fibonacci_hash = { 0 => 0, 1 => 1 }
 def fibonacci(index)
   if index >= 0 
-    if fibonacci_hash[index] != nil
-      return fibonacci_hash[index]
+    if $fibonacci_hash[index] != nil
+      return $fibonacci_hash[index]
     else
-      return fibonacci_hash[index] = fibonacci(index-1) + fibonacci(index-2)
+      return $fibonacci_hash[index] = fibonacci(index-1) + fibonacci(index-2)
     end
   else
     puts ("Invalid argument, index must be greater than or equal to 0")
@@ -13,8 +13,17 @@ end
 
 def populate_fibonacci
   terms = 0
-  until fibonacci(terms) > 4000000
-    puts (fibonacci(terms))
+  sum = 0
+  value = 0
+  until value > 4000000
+    puts (value)
+    if value % 2 == 0
+      sum += value
+    end
     terms += 1
+    value = fibonacci(terms)
   end
+  puts("Sum of even fibonacci numbers = #{sum}")
 end
+
+populate_fibonacci
